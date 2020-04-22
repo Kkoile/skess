95ab0642b8f6195b5a5db87ffcbc896b7fb4da4c
Nils Hirsekorn <Nils.Hirsekorn@sap.com>
2020-04-22 13:39:08 +0200

    better websocket handling

--- routes/io.js
+++ routes/io.js
@@ -5,23 +5,27 @@ module.exports = (io) => {
   io.adapter(Redis.getRedisAdapter());
 
   io.on('connection', (socket) => {
-    let userId;
     socket.on('login', async ({id, name}) => {
-      userId = id;
       await Redis.setItem(id, {id, socketId: socket.id, name, activeGames: []})
+      await Redis.setItem(socket.id, id);
     });
     socket.on('enterGame', async (data) => {
       socket.join(data.gameId);
+      const userId = await Redis.getItem(socket.id);
       await Game.enterGame(data.gameId, userId);
     });
     socket.on("disconnect", async () => {
-      const user = await Redis.getItem(userId);
-      for (const game of user.activeGames) {
-        await Game.leaveGame(game, user.id);
+      const userId = await Redis.getItem(socket.id);
+      if (userId) {
+        const user = await Redis.getItem(userId);
+        for (const game of user.activeGames) {
+          await Game.leaveGame(game, user.id);
+        }
+        user.socketId = null;
+        user.activeGames = [];
+        await Redis.setItem(userId, user);
+        await Redis.deleteItem(socket.id);
       }
-      user.socketId = null;
-      user.activeGames = [];
-      await Redis.setItem(userId, user);
     });
   })
 
